359b74311a3b5fff5a3cd3ef47c4e986d629f4d8
Pasang Lama <165545784+pasang111@users.noreply.github.com>
2026-07-21 18:40:05 +0800

    Refactor loo.py by commenting out initial code
    Comment out initial variable assignments and prints, and add conditional check for variable 'a'.

--- loo.py
+++ loo.py
@@ -1,6 +1,14 @@
-a = 5
-b = 10
-print(a)
-print(b)
+# a = 5
+# b = 10
+# print(a)
+# print(b)
 
-print(a+b)
+# print(a+b)
+
+
+a = 10
+
+if a <= 10:
+  print("you are hero")
+else:
+  print("you are zero")
